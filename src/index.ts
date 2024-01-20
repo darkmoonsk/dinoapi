751ba908c1a483a3ed0e282fd81cae08d4352321
Bruno Souza <brunoezully@gmail.com>
2024-01-20 02:43:11 -0300

    feat: initial server setup and dinosaur routes

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import { PORT } from "./constants";
+import connectToDatabase from "./config/database";
+import cors from "cors";
+
+import dinosaurRoutes from "./routes/dinosaurRoutes";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+connectToDatabase();
+
+app.use("/api/", dinosaurRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
