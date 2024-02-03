aa99a69c1f7c51834953c5d9785dd239059e536d
Bruno Souza <brunoezully@gmail.com>
2024-02-03 15:25:43 -0300

    refactor: dinosaurRoutes.ts to include validation and error handling

--- src/routes/dinosaurRoutes.ts
+++ src/routes/dinosaurRoutes.ts
@@ -1,11 +1,24 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { dinosaurParamsValidators } from "../validators/DinosaurParamsValidator";
 import dinosaurController from "../controllers/DinosaurController";
+import DinosaurParams from "../models/DinosaurParams";
+import replaceHyphensWithSpaces from "../utils/replaceHyphensWithSpaces";
+import Dinosaur from "../models/Dinosaur";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+
+router.get("/", dinosaurParamsValidators, async (req: Request, res: Response) => {
+  const erros = validationResult(req);
+  if (!erros.isEmpty()) {
+    return res.status(400).json({ message: "Invalid query parameters", errors: erros.array() });
+  }
+
+  const dinosaurParams: DinosaurParams = replaceHyphensWithSpaces(req.query);
+  
   try {
-    const dinosaurs = await dinosaurController.getAll();
+    const dinosaurs = await dinosaurController.getAll(dinosaurParams);
     res.status(200).json(dinosaurs);
   } catch (error) {
     console.error(error);
@@ -13,7 +26,25 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:name", async (req, res) => {
+
+router.get("/total", dinosaurParamsValidators, async (req: Request, res: Response) =>{
+  const erros = validationResult(req);
+  if (!erros.isEmpty()) {
+    return res.status(400).json({ message: "Invalid query parameters", errors: erros.array() });
+  }
+
+  const dinosaurParams: DinosaurParams = replaceHyphensWithSpaces(req.query);
+
+  try {
+    const total = await dinosaurController.count(dinosaurParams);
+    res.status(200).json({ total });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.get("/:name", async (req: Request, res: Response) => {
   try {
     const name = req.params.name.toLowerCase();
     const dinosaur = await dinosaurController.getByName(name);
@@ -24,17 +55,27 @@ router.get("/:name", async (req, res) => {
   }
 });
 
-// router.post("/", async (req, res) => {
-//   try {
-//     const dinosaur = req.body;
-//     await dinosaurController.create(dinosaur);
-//     res.status(201).json(dinosaur);
-//   } catch (error) {
-//     console.error(error);
-//     res
-//       .status(400)
-//       .json({ message: "Dinosaur record not created, check the data sent" });
-//   }
-// });
+router.post("/", async (req: Request, res: Response) => {
+  const secret = req.headers.secret;
+  const dinosaur = req.body;
+
+
+  if (secret !== process.env.API_SECRET) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+
+  if(!dinosaur || dinosaur != typeof Dinosaur) {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+
+  try {
+    await dinosaurController.create(dinosaur);
+    res.status(201).json(dinosaur);
+  } catch (error) {
+    console.error(error);
+    res.status(400)
+      .json({ message: "Dinosaur record not created, check the data sent" });
+  }
+});
 
 export default router;
